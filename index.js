f8553495e828a270d68b09e177f09e9ece580f65
eemj <jamiefarrugia@msn.com>
2017-06-13 02:26:38 +0200

    Add chat hooks

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ module.exports = function SaveTp(dispatch) {
 	let currLocation = null;
 	let savedLocation = null;
 
+	const chatHandler = event => {
+		const message = event.message.replace(/(<([^>]+)>)/ig, '');
+		switch (message) {
+			case '!loc':
+				dispatch.toClient('S_INSTANT_MOVE', 1, Object.assign(savedLocation, { id: cid }));
+				return false;
+			case '!save':
+				savedLocation = currLocation;
+				return false;
+			default:
+				break;
+		}
+	};
+
+	dispatch.hook('C_WHISPER', 1, chatHandler);
+	dispatch.hook('C_CHAT', 1, chatHandler);
+
 	dispatch.hook('S_LOGIN', 1, event => ({ cid } = event));
 
 	dispatch.hook('C_PLAYER_LOCATION', 1, event => {
@@ -21,10 +38,8 @@ module.exports = function SaveTp(dispatch) {
 			savedLocation = currLocation;
 			return false;
 		} else if (event.item === TP_BACK_TRIGGER_ITEM) {
-			dispatch.toClient('S_INSTANT_MOVE', 1, Object.assign(savedLocation, {
-				id: cid
-			}));
+			dispatch.toClient('S_INSTANT_MOVE', 1, Object.assign(savedLocation, { id: cid }));
 			return false;
 		}
 	});
-}
+};
